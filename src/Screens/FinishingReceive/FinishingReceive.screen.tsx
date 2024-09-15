641b4eed0518889301dd1c71a2e1038dcfaf2014
Sakkar Chowdhury <Sakkhor.Chowdhury@gmail.com>
2024-09-15 15:31:14 +0600

    TabBar Screen added

--- src/Screens/FinishingReceive/FinishingReceive.screen.tsx
+++ src/Screens/FinishingReceive/FinishingReceive.screen.tsx
@@ -1,85 +1,23 @@
-import React, {useState} from 'react';
-import {Dimensions, Image, Text, TouchableOpacity, View} from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
-import {SceneMap, TabBar, TabView} from 'react-native-tab-view';
+import React from 'react';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {useNavigation} from '@react-navigation/native';
 
 import ReceiveTab from '../../Components/ReceiveTab/ReceiveTab';
-
-import Styles from './Styles';
 import AlterResendTab from '@/Components/AlterResendTab/AlterResendTab';
 import TodaySummaryTab from '@/Components/TodaySummaryTab/TodaySummaryTab';
 import PendingTab from '@/Components/PendingTab/PendingTab';
 import FinishingAlterTab from '@/Components/FinishingAlterTab/FinishingAlterTab';
 
-// Define the routes' components
+import Styles from './Styles';
+import LinearGradient from 'react-native-linear-gradient';
 
+// Create Top Tab Navigator
+const Tab = createMaterialTopTabNavigator();
 
 const FinishingReceiveScreen = () => {
   const navigation = useNavigation();
-  const [index, setIndex] = useState(0);
-  const [routes] = useState([
-    {key: 'receive', title: 'RECEIVE', icon: 'arrow-circle-down'},
-    {key: 'pending', title: 'PENDING', icon: 'clock-o', count: 2},
-    {key: 'finishing', title: 'FINISHING ALTER', icon: 'scissors'},
-    {key: 'resend', title: 'ALTER RESEND', icon: 'repeat'},
-    {key: 'summary', title: 'TODAY SUMMARY', icon: 'calendar-check-o'},
-  ]);
-
-  const FirstRoute: React.FC = () => {
-    return (
-      <>
-        <ReceiveTab />
-      </>
-    );
-  };
-  const SecondRoute: React.FC = () => <PendingTab/>;
-  const ThirdRoute: React.FC = () => <FinishingAlterTab/>;
-  const FourthRoute: React.FC = () => <AlterResendTab/>;
-  const FifthRoute: React.FC = () => <TodaySummaryTab/>;
-
-
-  const renderScene = SceneMap({
-    receive: FirstRoute,
-    pending: SecondRoute,
-    finishing: ThirdRoute,
-    resend: FourthRoute,
-    summary: FifthRoute,
-  });
-  
-
-  const renderTabBar = props => (
-    <TabBar
-      indicatorContainerStyle={Styles.indicatorStyle}
-      // tabStyle={Styles.tabStyle2}
-      tabStyle={Styles.tabStyle2}
-      {...props}
-      renderIcon={({route, focused, color}) => (
-        <View style={Styles.tabIconContainer}>
-          <Icon
-            name={route.icon}
-            size={20}
-            color={focused ? '#1C98D8' : '#555'}
-          />
-        </View>
-      )}
-      renderLabel={({route, focused}) => (
-        <View style={Styles.tabLabelContainer}>
-          <Text style={[Styles.label, focused && Styles.focusedLabel]}>
-            {route.title}
-          </Text>
-          {route?.count && (
-            <View style={Styles.counter}>
-              <Text style={Styles.counterText}>{route?.count}</Text>
-            </View>
-          )}
-        </View>
-      )}
-      indicatorStyle={Styles.indicator}
-      style={Styles.tabBar}
-    />
-  );
 
   return (
     <>
@@ -107,16 +45,53 @@ const FinishingReceiveScreen = () => {
           />
         </View>
       </LinearGradient>
-      <TabView
-        lazy
-        navigationState={{index, routes}}
-        renderScene={renderScene}
-        onIndexChange={setIndex}
-        initialLayout={{width: Dimensions.get('window').width}}
-        renderTabBar={renderTabBar}
-        style={Styles.TabStyle}
-        swipeEnabled={true}
-      />
+
+      <Tab.Navigator
+      style={{margin: 20, borderRadius: 20}}
+        initialRouteName="Receive"
+        tabBarOptions={{
+          showIcon: true,
+          style: Styles.tabBar,
+          indicatorStyle: Styles.indicator,
+        }}
+        screenOptions={({route}) => ({
+          title: route.name,
+          tabBarActiveTintColor:'#898792',
+          tabBarInactiveTintColor: '#555',
+          tabBarItemStyle: {flexDirection:'row'},
+          tabBarIcon: ({focused, color}) => {
+            let iconName;
+            switch (route.name) {
+              case 'Receive':
+                iconName = 'arrow-circle-down';
+                break;
+              case 'Pending':
+                iconName = 'clock-o';
+                break;
+              case 'Finishing Alter':
+                iconName = 'scissors';
+                break;
+              case 'F. Alter Resend':
+                iconName = 'repeat';
+                break;
+              case 'Today Summary':
+                iconName = 'calendar-check-o';
+                break;
+              default:
+                iconName = 'question';
+            }
+            return (
+              <Icon name={iconName} size={20} color={focused ? '#1C98D8' : '#555'} />
+            );
+          },
+        })}
+      >
+        <Tab.Screen name="Receive" component={ReceiveTab} />
+        <Tab.Screen name="Pending" component={PendingTab} />
+        <Tab.Screen name="Finishing Alter" component={FinishingAlterTab} />
+        <Tab.Screen name="F. Alter Resend" component={AlterResendTab} />
+        <Tab.Screen name="Today Summary" component={TodaySummaryTab} />
+      </Tab.Navigator>
     </>
   );
 };
