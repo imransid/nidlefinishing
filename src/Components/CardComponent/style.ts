eae11adbd42fc5b1a70bceea17f3a02b3fbcad73
Sakkar Chowdhury <Sakkhor.Chowdhury@gmail.com>
2024-09-18 13:19:23 +0600

    NIDLE-FINISHING-100: Design Modified

--- src/Components/CardComponent/style.ts
+++ src/Components/CardComponent/style.ts
@@ -1,81 +1,86 @@
-import {StyleSheet} from 'react-native';
-import {scale, moderateScale, verticalScale} from 'react-native-size-matters';
+/* eslint-disable react-native/no-color-literals */
+
+import { StyleSheet } from 'react-native';
+import { moderateScale, scale, verticalScale } from 'react-native-size-matters';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  cardContentStyle: {
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: '#EEEEEE',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 10
+  },
+  cardHeaderContainer: {
+    alignItems: 'center'
   },
   cardHeading: {
     color: '#765492',
     fontSize: moderateScale(13),
     fontWeight: '700',
     textAlign: 'center',
-    width: '100%',
+    width: '100%'
   },
   cardStyle: {
     backgroundColor: '#F9F8FB',
-    borderWidth: 1,
-    borderColor: '#E3E1F0',
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
+    borderColor: '#E3E1F0',
+    borderWidth: 1
   },
-  cardContentStyle: {
-    borderColor: '#EEEEEE',
-    borderBottomWidth: 1,
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  lineContentStyle: {
-    backgroundColor: '#1C98D8',
-    borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-    height:verticalScale(15),
-    alignContent:"center"
-  },
-  lineText: {
-    color: '#fff',
-    fontSize: moderateScale(7),
+  container: {
+    flex: 1
   },
   informationContentStyle: {
     flexDirection: 'column',
-    paddingHorizontal: scale(5),
+    paddingHorizontal: scale(5)
+  },
+  lineContentStyle: {
+    alignContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1C98D8',
+    borderRadius: 5,
+    height: verticalScale(15),
+    justifyContent: 'center'
+  },
+  lineText: {
+    color: '#fff',
+    fontSize: moderateScale(7)
   },
   receiveInfoText: {
-    fontSize: moderateScale(8),
-    fontWeight: '500',
     color: '#444444',
+    fontSize: moderateScale(8),
+    fontWeight: '500'
   },
   sizeContentStyle: {
     flexDirection: 'row',
-    gap: 10,
+    gap: 10
   },
   sizeQtyText: {
-    fontSize: moderateScale(7),
-    fontWeight: '500',
     color: '#765492',
-  },
-  statusTextConfirm: {
     fontSize: moderateScale(7),
-    fontWeight: '600',
-    color: '#4BB543',
-    alignSelf:'center'
+    fontWeight: '500'
   },
   statusTextCancel: {
-    fontSize: moderateScale(7),
-    fontWeight: '600',
+    alignSelf: 'center',
     color: '#E62E2D',
-    alignSelf:'center'
+    fontSize: moderateScale(7),
+    fontWeight: '600'
+  },
+  statusTextConfirm: {
+    alignSelf: 'center',
+    color: '#4BB543',
+    fontSize: moderateScale(7),
+    fontWeight: '600'
   },
   statusTextPending: {
-    fontSize: moderateScale(8),
-    fontWeight: '600',
+    alignSelf: 'center',
     color: '#D1B00D',
-    alignSelf:'center'
-  },
+    fontSize: moderateScale(8),
+    fontWeight: '600'
+  }
 });
 
 export default styles;
